c9745821773530f1b558ddeea3603808ddc86ee8
Azalea Mezzo <mezzoEmrys@gmail.com>
2024-08-03 17:00:10 -0500

    tweaked jump logic

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 @export var speed : float = 300.0
-@export var jump_force : float = -250.0
-@export var jump_time : float = 0.25
+@export var jump_impulse : float = -400.0
+@export var jump_sustain : float = -0.5
+@export var jump_time : float = 0.2
 @export var coyote_time : float = 0.075
 @export var gravity_multiplier : float = 3.0
 
@@ -28,28 +29,28 @@ func finish_charge():
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor() and not is_jumping:
-		velocity.y += gravity * delta
-		coyote_timer += delta
-	elif is_on_floor():
-		coyote_timer = 0
-		
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_timer < coyote_time) and not is_jumping:
-		is_charging = true
-		is_jumping = true
-	elif Input.is_action_pressed("jump") and not is_charging and is_jumping:
-		velocity.y = jump_force
+	if not is_jumping:
+		if not is_on_floor() :
+			velocity.y += gravity * delta
+			coyote_timer += delta
+		elif is_on_floor():
+			coyote_timer = 0
+			jump_timer = 0
 	
-	if is_jumping and Input.is_action_pressed("jump") and jump_timer < jump_time:
-		jump_timer += delta
-	else:
-		#TODO: These two lines of code make the character drop immediately after input release
-		#if (velocity.y < -100):
-			#velocity.y = -100
-		is_jumping = false
-		jump_timer = 0
-
+	if is_on_floor() or (coyote_timer < coyote_time) :
+		if Input.is_action_just_pressed("jump") :
+			is_charging = true
+			is_jumping = true
+			coyote_timer = coyote_time
+		if is_jumping and not is_charging :
+			velocity.y += jump_impulse
+	elif is_jumping:
+		if Input.is_action_pressed("jump") and jump_timer < jump_time:
+			velocity.y += jump_sustain
+			jump_timer += delta
+		else :
+			is_jumping = false
+			
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
