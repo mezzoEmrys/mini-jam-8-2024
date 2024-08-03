a6a483074819cccb81f1075586c82f33a679e345
feedbackinc <feedb4ckinc@gmail.com>
2024-08-03 17:05:14 -0400

    bnuy

--- scenes/character_controller.gd
+++ scenes/character_controller.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 300.0
 @export var jump_force : float = -250.0
 @export var jump_time : float = 0.25
@@ -12,7 +11,22 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping : bool = false
 var jump_timer : float = 0
 var coyote_timer : float = 0
-@onready var sprite_2d = $GorpSprite
+#@onready var sprite_2d = $GorpSprite
+@onready var item_sprite = $held_item
+
+@onready var item_held = false
+@onready var item = ""
+
+#func _ready() -> void():
+	#connect("item_pickup", self, "")
+
+var is_charging : bool = false
+@onready var sprite_2d = $AnimatedSprite2D
+@onready var anim_player = $AnimationPlayer
+
+func finish_charge():
+	is_charging = false
+	velocity.y = jump_force * 2
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,12 +35,12 @@ func _physics_process(delta):
 		coyote_timer += delta
 	else:
 		coyote_timer = 0
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_timer < coyote_time):
-		velocity.y = jump_force
+		is_charging = true
 		is_jumping = true
-	elif Input.is_action_pressed("jump") and is_jumping:
+	elif Input.is_action_pressed("jump") and not is_charging and is_jumping:
 		velocity.y = jump_force
 	
 	if is_jumping and Input.is_action_pressed("jump") and jump_timer < jump_time:
@@ -42,11 +56,24 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 
-	if direction:
+	if direction != 0 and not is_charging:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-	
+			
+
 	if direction != 0:
 		sprite_2d.flip_h = direction < 0
 	move_and_slide()
+
+	if is_jumping :
+		anim_player.play("jumping")
+	elif is_on_floor() :
+		if velocity.x == 0 :
+			anim_player.play("idle")
+		else :
+			anim_player.play("walking")
+	elif not is_jumping : 
+		anim_player.play("falling")
+	
+			
