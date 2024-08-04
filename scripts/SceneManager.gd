e7126509acb82e16805bda3134abb045680b13e4
Azalea Mezzo <mezzoEmrys@gmail.com>
2024-08-04 15:17:14 -0500

    scene manager stuff

---
+++ scripts/SceneManager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@onready var level_container = $"../LevelScene"
+
+func _ready():
+	pass
+	
+func start_transition():
+	pass
+	
+func end_transition():
+	pass
+	
+func load_scene(scene: PackedScene):
+	pass
+	
