98547579b83c2971740b194d1455171cf0e06b49
Azalea Mezzo <mezzoEmrys@gmail.com>
2024-08-04 18:27:32 -0500

    cat paw

--- scripts/SceneManager.gd
+++ scripts/SceneManager.gd
@@ -2,6 +2,12 @@ extends Node
 
 @onready var level_container = $"../LevelScene"
 
+var catmint_active : bool = false
+var ginger_active : bool = false
+var ice_active : bool = false
+var lemon_active: bool = false
+var pepper_active: bool = false
+
 func _ready():
 	pass
 	
