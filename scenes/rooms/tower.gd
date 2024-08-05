3f1f390dd0c1af4a40bfec72328d164e7a1ecaca
feedbackinc <mr929@cornell.edu>
2024-08-05 01:20:11 -0400

    added icons

--- scenes/rooms/tower.gd
+++ scenes/rooms/tower.gd
@@ -8,14 +8,30 @@ func _ready():
 	
 	if not Get.SceneManager.catmint_active:
 		remove_child($Paw)
+	else:
+		remove_child($catmint-icon)
 	if not Get.SceneManager.ginger_active:
 		remove_child($Ghost)
+	else:
+		remove_child($ginger-icon)
 	if not Get.SceneManager.ice_active:
 		remove_child($ice)
+	else:
+		remove_child($ice-icon)
 	if not Get.SceneManager.pepper_active:
 		remove_child($pepper)
+	else:
+		remove_child($pepper-icon)
 	if not Get.SceneManager.lemon_active:
 		remove_child($lemon)
+	else:
+		remove_child($lemon-icon)
+	
+	if Get.SceneManager.tea_active:
+		remove_child($tea-icon)
+
+	if Get.SceneManager.mug_active:
+		remove_child($mug-icon)
 		
 	pass # Replace with function body.
 
