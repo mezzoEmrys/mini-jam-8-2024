cd73e0a0dbdaec78c5b5f55e5296421f42cd64a2
Azalea Mezzo <mezzoEmrys@gmail.com>
2024-08-04 21:10:04 -0500

    new ui

---
+++ scripts/ui_only.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("jump"):
+		_on_button_pressed()
+
+func _on_button_pressed():
+	Get.SceneManager.load_scene(load("res://scenes/main_menu.tscn"))
